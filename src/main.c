657e75294e98ef745c7fb7cc13401dc374dcd7fa
chethansk123 <skchethan123@gmail.com>
2022-02-23 14:05:11 +0100

    Initial commit

--- src/main.c
+++ src/main.c
@@ -59,7 +59,7 @@
 #define I2C_MASTER_SCL_IO           22                        /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO           21                        /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_FREQ_HZ          90000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_NUM              0
+#define I2C_MASTER_NUM              0  
 #define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS       1000
